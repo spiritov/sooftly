d9ef391b200bea0e6c7da6d0ed6d51ed2b393cd5
spiritov <132454615+spiritov@users.noreply.github.com>
2025-04-15 20:44:45 -0700

    fix background

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -1726,7 +1726,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1iy73w2"
+  version_hash: "1mydslx"
 };
 async function get_hooks() {
   let handle;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -107,329 +107,331 @@
 	}
 </script>
 
-<div class="w-full justify-center">
-	<div class="mt-8 w-2xl flex-col rounded-md bg-zinc-300/10 p-4 text-center text-lg">
-		<div class="mb-8 justify-center text-violet-300">tourney overlay helper</div>
-		{#key arrow}
-			<button onclick={toggleAdd} class="flex w-96 cursor-pointer self-center"
-				>{arrow} toggle</button
-			>
-		{/key}
-		{#if add}
-			<div transition:slide={{ duration: 150 }} class="w-96 flex-col gap-1 self-center">
-				{#each categories as [cat, array]}
-					<div class="relative justify-center">
-						{#if cat == 'flag'}
-							<a target="_blank" href="https://countryflagsapi.netlify.app/country-list">
-								<div class="absolute left-0 transition-colors hover:text-violet-300">
-									add {cat}
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 20 20"
-										fill="currentColor"
-										class="ml-1 flex size-5 h-full self-center"
-									>
-										<path
-											d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z"
-										/>
-										<path
-											d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z"
-										/>
-									</svg>
-								</div>
-							</a>
-						{:else}
-							<div class="absolute left-0">add {cat}</div>
-						{/if}
-						<button
-							onclick={() => {
-								categories.set(cat, []);
-								localStorage.removeItem(cat);
-							}}
-							class="absolute right-0 cursor-pointer transition-colors hover:text-red-400 hover:underline"
-						>
-							remove all
-						</button>
-						{#if cat == 'flag'}
-							<input
-								placeholder="alpha-2 code"
-								class="input"
-								onkeypress={(e) => {
-									if (e.key == 'Enter') {
-										if (e.target.value.length === 2) {
-											categories.set(
-												cat,
-												categories.get(cat).concat([e.target.value.toUpperCase()])
-											);
-											localStorage.setItem(cat, JSON.stringify(categories.get(cat)));
+<div class="block">
+	<div class="w-full justify-center">
+		<div class="mt-8 w-2xl flex-col rounded-md bg-zinc-300/10 p-4 text-center text-lg">
+			<div class="mb-8 justify-center text-violet-300">tourney overlay helper</div>
+			{#key arrow}
+				<button onclick={toggleAdd} class="flex w-96 cursor-pointer self-center"
+					>{arrow} toggle</button
+				>
+			{/key}
+			{#if add}
+				<div transition:slide={{ duration: 150 }} class="w-96 flex-col gap-1 self-center">
+					{#each categories as [cat, array]}
+						<div class="relative justify-center">
+							{#if cat == 'flag'}
+								<a target="_blank" href="https://countryflagsapi.netlify.app/country-list">
+									<div class="absolute left-0 transition-colors hover:text-violet-300">
+										add {cat}
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 20 20"
+											fill="currentColor"
+											class="ml-1 flex size-5 h-full self-center"
+										>
+											<path
+												d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z"
+											/>
+											<path
+												d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z"
+											/>
+										</svg>
+									</div>
+								</a>
+							{:else}
+								<div class="absolute left-0">add {cat}</div>
+							{/if}
+							<button
+								onclick={() => {
+									categories.set(cat, []);
+									localStorage.removeItem(cat);
+								}}
+								class="absolute right-0 cursor-pointer transition-colors hover:text-red-400 hover:underline"
+							>
+								remove all
+							</button>
+							{#if cat == 'flag'}
+								<input
+									placeholder="alpha-2 code"
+									class="input"
+									onkeypress={(e) => {
+										if (e.key == 'Enter') {
+											if (e.target.value.length === 2) {
+												categories.set(
+													cat,
+													categories.get(cat).concat([e.target.value.toUpperCase()])
+												);
+												localStorage.setItem(cat, JSON.stringify(categories.get(cat)));
+											}
+											e.target.value = '';
 										}
-										e.target.value = '';
-									}
-									inputs.set(cat, e.target.value);
-								}}
-							/>
-						{:else}
-							<input
-								class="input"
-								onkeypress={(e) => {
-									if (e.key == 'Enter') {
-										categories.set(cat, categories.get(cat).concat([e.target.value]));
-										localStorage.setItem(cat, JSON.stringify(categories.get(cat)));
-										e.target.value = '';
-									}
-									inputs.set(cat, e.target.value);
-								}}
-							/>
-						{/if}
+										inputs.set(cat, e.target.value);
+									}}
+								/>
+							{:else}
+								<input
+									class="input"
+									onkeypress={(e) => {
+										if (e.key == 'Enter') {
+											categories.set(cat, categories.get(cat).concat([e.target.value]));
+											localStorage.setItem(cat, JSON.stringify(categories.get(cat)));
+											e.target.value = '';
+										}
+										inputs.set(cat, e.target.value);
+									}}
+								/>
+							{/if}
+						</div>
+					{/each}
+					<div class="gap-2">
+						<span>font</span>
+						<button
+							class="button {sel_font === 0 ? 'bg-violet-300' : ''}"
+							onclick={() => {
+								sel_font = 0;
+								localStorage.setItem('font', 'mono');
+							}}>Ubuntu Mono</button
+						>
+						<button
+							class="font-outfit button {sel_font === 1 ? 'bg-violet-300' : ''}"
+							onclick={() => {
+								sel_font = 1;
+								localStorage.setItem('font', 'outfit');
+							}}>Outfit</button
+						>
 					</div>
+				</div>
+			{/if}
+
+			<div class="relative mt-8 w-96 justify-center gap-2 self-center">
+				<span class="absolute left-0">best of</span>
+				{#each { length: 5 }, i}
+					<button
+						class="button size-8 justify-center px-0 {i === sel_best_of
+							? 'border-violet-400 bg-violet-300'
+							: ''}"
+						onclick={() => {
+							sel_best_of = i;
+							localStorage.setItem('max_score', i + 1);
+						}}>{i * 2 + 1}</button
+					>
 				{/each}
-				<div class="gap-2">
-					<span>font</span>
+			</div>
+
+			<div>map</div>
+			<div class="flex-wrap gap-2">
+				{#each categories.get('map') as map, i}
+					{#if sel_map === i}
+						<RemovableItem
+							name={map}
+							sel={true}
+							onSelect={() => {
+								sel_map = i;
+								localStorage.setItem('overlay_map', map);
+							}}
+							onRemove={(e) => removeItem(e, 'map', i)}
+						/>
+					{:else}
+						<RemovableItem
+							name={map}
+							onSelect={() => {
+								sel_map = i;
+								localStorage.setItem('overlay_map', map);
+							}}
+							onRemove={(e) => removeItem(e, 'map', i)}
+						/>
+					{/if}
+				{/each}
+			</div>
+
+			<div class="mt-4 flex-row justify-between">
+				<div class="basis-1/2 flex-col border-r-2 border-zinc-300/10 pr-2">
+					<div class="justify-center">left player</div>
+					<div class="flex-wrap gap-2">
+						{#each categories.get('player') as player, i}
+							{#if sel_left_player === i}
+								<RemovableItem
+									name={player}
+									sel={true}
+									onSelect={() => {
+										sel_left_player = i;
+										localStorage.setItem('left_player', left_player);
+									}}
+									onRemove={(e) => removeItem(e, 'player', i)}
+								/>
+							{:else}
+								<RemovableItem
+									name={player}
+									onSelect={() => {
+										sel_left_player = i;
+										localStorage.setItem('left_player', left_player);
+									}}
+									onRemove={(e) => removeItem(e, 'player', i)}
+								/>
+							{/if}
+						{/each}
+					</div>
+
+					<div class="justify-center">flag</div>
+					<div class="flex-wrap gap-2">
+						{#each categories.get('flag') as flag, i}
+							{#if sel_left_flag === i}
+								<RemovableItem
+									name={flag}
+									isFlag={true}
+									sel={true}
+									onSelect={() => {
+										sel_left_flag = i;
+										localStorage.setItem('left_flag', left_flag);
+									}}
+									onRemove={(e) => removeItem(e, 'flag', i)}
+								/>
+							{:else}
+								<RemovableItem
+									name={flag}
+									isFlag={true}
+									onSelect={() => {
+										sel_left_flag = i;
+										localStorage.setItem('left_flag', left_flag);
+									}}
+									onRemove={(e) => removeItem(e, 'flag', i)}
+								/>
+							{/if}
+						{/each}
+					</div>
+
+					<div class="mt-4">
+						<div>map pr:</div>
+						<input
+							bind:value={left_pr}
+							class="input"
+							oninput={(e) => localStorage.setItem('left_pr', e.target.value)}
+						/>
+					</div>
+				</div>
+
+				<div class="basis-1/2 flex-col justify-items-center pl-2">
+					<div class="justify-center">right player</div>
+					<div class="flex-wrap gap-2">
+						{#each categories.get('player') as player, i}
+							{#if sel_rght_player === i}
+								<RemovableItem
+									name={player}
+									sel={true}
+									onSelect={() => {
+										sel_rght_player = i;
+										localStorage.setItem('rght_player', rght_player);
+									}}
+									onRemove={(e) => removeItem(e, 'player', i)}
+								/>
+							{:else}
+								<RemovableItem
+									name={player}
+									onSelect={() => {
+										sel_rght_player = i;
+										localStorage.setItem('rght_player', rght_player);
+									}}
+									onRemove={(e) => removeItem(e, 'player', i)}
+								/>
+							{/if}
+						{/each}
+					</div>
+
+					<div class="justify-center">flag</div>
+					<div class="flex-wrap gap-2">
+						{#each categories.get('flag') as flag, i}
+							{#if sel_rght_flag === i}
+								<RemovableItem
+									name={flag}
+									isFlag={true}
+									sel={true}
+									onSelect={() => {
+										sel_rght_flag = i;
+										localStorage.setItem('rght_flag', rght_flag);
+									}}
+									onRemove={(e) => removeItem(e, 'flag', i)}
+								/>
+							{:else}
+								<RemovableItem
+									name={flag}
+									isFlag={true}
+									onSelect={() => {
+										sel_rght_flag = i;
+										localStorage.setItem('rght_flag', rght_flag);
+									}}
+									onRemove={(e) => removeItem(e, 'flag', i)}
+								/>
+							{/if}
+						{/each}
+					</div>
+
+					<div class="mt-4">
+						<div>map pr:</div>
+						<input
+							bind:value={rght_pr}
+							class="input"
+							oninput={(e) => localStorage.setItem('rght_pr', e.target.value)}
+						/>
+					</div>
+				</div>
+			</div>
+
+			<div class="relative mt-4 justify-center">
+				<div class="w-full justify-center">
 					<button
-						class="button {sel_font === 0 ? 'bg-violet-300' : ''}"
-						onclick={() => {
-							sel_font = 0;
-							localStorage.setItem('font', 'mono');
-						}}>Ubuntu Mono</button
+						class="button size-8 justify-center rounded-r-none px-0"
+						onclick={() => editScore(true, left_score + 1)}>+</button
+					>
+					<button class="button size-8 justify-center rounded-none bg-violet-300 px-0"
+						>{left_score}</button
 					>
 					<button
-						class="font-outfit button {sel_font === 1 ? 'bg-violet-300' : ''}"
-						onclick={() => {
-							sel_font = 1;
-							localStorage.setItem('font', 'outfit');
-						}}>Outfit</button
+						class="button size-8 justify-center rounded-l-none px-0"
+						onclick={() => editScore(true, left_score - 1)}>-</button
+					>
+				</div>
+				<div class="absolute">score</div>
+				<div class="w-full justify-center">
+					<button
+						class="button size-8 justify-center rounded-r-none px-0"
+						onclick={() => editScore(false, rght_score + 1)}>+</button
+					>
+					<button class="button size-8 justify-center rounded-none bg-violet-300 px-0"
+						>{rght_score}</button
+					>
+					<button
+						class="button size-8 justify-center rounded-l-none px-0"
+						onclick={() => editScore(false, rght_score - 1)}>-</button
 					>
 				</div>
 			</div>
-		{/if}
 
-		<div class="relative mt-8 w-96 justify-center gap-2 self-center">
-			<span class="absolute left-0">best of</span>
-			{#each { length: 5 }, i}
-				<button
-					class="button size-8 justify-center px-0 {i === sel_best_of
-						? 'border-violet-400 bg-violet-300'
-						: ''}"
-					onclick={() => {
-						sel_best_of = i;
-						localStorage.setItem('max_score', i + 1);
-					}}>{i * 2 + 1}</button
-				>
-			{/each}
-		</div>
-
-		<div>map</div>
-		<div class="flex-wrap gap-2">
-			{#each categories.get('map') as map, i}
-				{#if sel_map === i}
-					<RemovableItem
-						name={map}
-						sel={true}
-						onSelect={() => {
-							sel_map = i;
-							localStorage.setItem('overlay_map', map);
-						}}
-						onRemove={(e) => removeItem(e, 'map', i)}
-					/>
-				{:else}
-					<RemovableItem
-						name={map}
-						onSelect={() => {
-							sel_map = i;
-							localStorage.setItem('overlay_map', map);
-						}}
-						onRemove={(e) => removeItem(e, 'map', i)}
-					/>
-				{/if}
-			{/each}
-		</div>
-
-		<div class="mt-4 flex-row justify-between">
-			<div class="basis-1/2 flex-col border-r-2 border-zinc-300/10 pr-2">
-				<div class="justify-center">left player</div>
-				<div class="flex-wrap gap-2">
-					{#each categories.get('player') as player, i}
-						{#if sel_left_player === i}
-							<RemovableItem
-								name={player}
-								sel={true}
-								onSelect={() => {
-									sel_left_player = i;
-									localStorage.setItem('left_player', left_player);
-								}}
-								onRemove={(e) => removeItem(e, 'player', i)}
-							/>
-						{:else}
-							<RemovableItem
-								name={player}
-								onSelect={() => {
-									sel_left_player = i;
-									localStorage.setItem('left_player', left_player);
-								}}
-								onRemove={(e) => removeItem(e, 'player', i)}
-							/>
-						{/if}
-					{/each}
-				</div>
-
-				<div class="justify-center">flag</div>
-				<div class="flex-wrap gap-2">
-					{#each categories.get('flag') as flag, i}
-						{#if sel_left_flag === i}
-							<RemovableItem
-								name={flag}
-								isFlag={true}
-								sel={true}
-								onSelect={() => {
-									sel_left_flag = i;
-									localStorage.setItem('left_flag', left_flag);
-								}}
-								onRemove={(e) => removeItem(e, 'flag', i)}
-							/>
-						{:else}
-							<RemovableItem
-								name={flag}
-								isFlag={true}
-								onSelect={() => {
-									sel_left_flag = i;
-									localStorage.setItem('left_flag', left_flag);
-								}}
-								onRemove={(e) => removeItem(e, 'flag', i)}
-							/>
-						{/if}
-					{/each}
-				</div>
-
-				<div class="mt-4">
-					<div>map pr:</div>
-					<input
-						bind:value={left_pr}
-						class="input"
-						oninput={(e) => localStorage.setItem('left_pr', e.target.value)}
-					/>
-				</div>
+			<div>stage</div>
+			<div class="flex-wrap gap-2">
+				{#each categories.get('stage') as stage, i}
+					{#if sel_stage === i}
+						<RemovableItem
+							name={stage}
+							sel={true}
+							onSelect={() => {
+								sel_stage = i;
+								localStorage.setItem('overlay_stage', stage);
+							}}
+							onRemove={(e) => removeItem(e, 'stage', i)}
+						/>
+					{:else}
+						<RemovableItem
+							name={stage}
+							onSelect={() => {
+								sel_stage = i;
+								localStorage.setItem('overlay_stage', stage);
+							}}
+							onRemove={(e) => removeItem(e, 'stage', i)}
+						/>
+					{/if}
+				{/each}
 			</div>
-
-			<div class="basis-1/2 flex-col justify-items-center pl-2">
-				<div class="justify-center">right player</div>
-				<div class="flex-wrap gap-2">
-					{#each categories.get('player') as player, i}
-						{#if sel_rght_player === i}
-							<RemovableItem
-								name={player}
-								sel={true}
-								onSelect={() => {
-									sel_rght_player = i;
-									localStorage.setItem('rght_player', rght_player);
-								}}
-								onRemove={(e) => removeItem(e, 'player', i)}
-							/>
-						{:else}
-							<RemovableItem
-								name={player}
-								onSelect={() => {
-									sel_rght_player = i;
-									localStorage.setItem('rght_player', rght_player);
-								}}
-								onRemove={(e) => removeItem(e, 'player', i)}
-							/>
-						{/if}
-					{/each}
-				</div>
-
-				<div class="justify-center">flag</div>
-				<div class="flex-wrap gap-2">
-					{#each categories.get('flag') as flag, i}
-						{#if sel_rght_flag === i}
-							<RemovableItem
-								name={flag}
-								isFlag={true}
-								sel={true}
-								onSelect={() => {
-									sel_rght_flag = i;
-									localStorage.setItem('rght_flag', rght_flag);
-								}}
-								onRemove={(e) => removeItem(e, 'flag', i)}
-							/>
-						{:else}
-							<RemovableItem
-								name={flag}
-								isFlag={true}
-								onSelect={() => {
-									sel_rght_flag = i;
-									localStorage.setItem('rght_flag', rght_flag);
-								}}
-								onRemove={(e) => removeItem(e, 'flag', i)}
-							/>
-						{/if}
-					{/each}
-				</div>
-
-				<div class="mt-4">
-					<div>map pr:</div>
-					<input
-						bind:value={rght_pr}
-						class="input"
-						oninput={(e) => localStorage.setItem('rght_pr', e.target.value)}
-					/>
-				</div>
-			</div>
-		</div>
-
-		<div class="relative mt-4 justify-center">
-			<div class="w-full justify-center">
-				<button
-					class="button size-8 justify-center rounded-r-none px-0"
-					onclick={() => editScore(true, left_score + 1)}>+</button
-				>
-				<button class="button size-8 justify-center rounded-none bg-violet-300 px-0"
-					>{left_score}</button
-				>
-				<button
-					class="button size-8 justify-center rounded-l-none px-0"
-					onclick={() => editScore(true, left_score - 1)}>-</button
-				>
-			</div>
-			<div class="absolute">score</div>
-			<div class="w-full justify-center">
-				<button
-					class="button size-8 justify-center rounded-r-none px-0"
-					onclick={() => editScore(false, rght_score + 1)}>+</button
-				>
-				<button class="button size-8 justify-center rounded-none bg-violet-300 px-0"
-					>{rght_score}</button
-				>
-				<button
-					class="button size-8 justify-center rounded-l-none px-0"
-					onclick={() => editScore(false, rght_score - 1)}>-</button
-				>
-			</div>
-		</div>
-
-		<div>stage</div>
-		<div class="flex-wrap gap-2">
-			{#each categories.get('stage') as stage, i}
-				{#if sel_stage === i}
-					<RemovableItem
-						name={stage}
-						sel={true}
-						onSelect={() => {
-							sel_stage = i;
-							localStorage.setItem('overlay_stage', stage);
-						}}
-						onRemove={(e) => removeItem(e, 'stage', i)}
-					/>
-				{:else}
-					<RemovableItem
-						name={stage}
-						onSelect={() => {
-							sel_stage = i;
-							localStorage.setItem('overlay_stage', stage);
-						}}
-						onRemove={(e) => removeItem(e, 'stage', i)}
-					/>
-				{/if}
-			{/each}
 		</div>
 	</div>
 </div>
